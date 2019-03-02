dddb2629103973eee1c275b64baeacf2a33ea525
Akeena Hall <akeenadhall@gmail.com>
2019-03-02 16:04:22 -0500

    learning jQuery

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var setupimg = document.getElementById("img");
-setupimg.addEventListener("click" ,myEventHandler);
+$(document).ready(function() {
+	//This line of code will change all h1 tags to red when hovered over, and they will return to white once the mouse is no longer hovering over the text
+	$('h1').hover(on,off);
+		function on (){
+		$(this).css("color","red");
+		}
+		function off(){
+		$(this).css("color","white");
+		}
+	$("#img").hover(enter,leave);
+		function enter(){
+			this.src = "ombre.jpg";
+		}
+		function leave(){
+			this.src = "computerClassSetup.jpg";
+		}
 
-function myEventHandler(){
- 	setupimg.setAttribute("src","computerClassFlyer.PNG");
-}
 
+})
