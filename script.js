6fed18e831903e835d3d518f42973cc474fa32a0
Akeena Hall <akeenadhall@gmail.com>
2019-04-13 15:58:37 -0400

    ADDING MY SLIDESHOOOWWWWWW

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+
+// slideshow; taking code from w3Schools
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+//
 $(document).ready(function() {
 	//This line of code will change all h1 tags to red when hovered over, and they will return to white once the mouse is no longer hovering over the text
 	$('h1').hover(on,off);
@@ -6,13 +35,6 @@ $(document).ready(function() {
 		}
 		function off(){
 		$(this).css("color","white");
-		}
-	$("#img").hover(enter,leave);
-		function enter(){
-			this.src = "ombre.jpg";
-		}
-		function leave(){
-			this.src = "computerClassSetup.jpg";
 		}
 	//this code will make the divs, about me, contact, etc, toggle down and back up 
 	$('#aboutMeButton').click(function(){
@@ -26,3 +48,7 @@ $(document).ready(function() {
 
 	});
 })
+
+
+
+
