d28e583c1e5ac954e9ee87b35a0adb2ca49066b5
akeenahall42029 <akeenadhall@gmail.com>
2021-01-23 14:02:00 -0500

    I still need to finisht step 2 for challenge 2 of the rainbow express; this is the code that renders one template for the seven colors of the rainbow

---
+++ rainbow_templates/app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, current_app as current_app
+
+app = Flask(__name__)
+
+
+@app.route('/')   
+def index():
+    return "Welcome to Akeena's Rainbow Project"
+@app.route('/rainbow')
+def rainbow():
+    return "Currently in the works"
+    
+@app.route('/red', methods=['GET'])
+def redPage():
+    color = 'RED'
+    return render_template("colors.html",color = color)
+
+@app.route('/orange',methods=['GET'])
+def orangePage():
+    color = 'ORANGE'
+    return render_template("colors.html",color=color)
+
+@app.route('/yellow',methods=['GET'])
+def yellowPage():
+    color = 'YELLOW'
+    return render_template("colors.html",color=color)
+
+@app.route('/green',methods=['GET'])
+def greenPage():
+    color = 'GREEN'
+    return render_template("colors.html", color=color)
+
+@app.route('/blue',methods=['GET'])
+def bluePage():
+    color = 'BLUE'
+    return render_template("colors.html", color=color)
+
+@app.route('/indigo',methods=['GET'])
+def indigoPage():
+    color = 'INDIGO'
+    return render_template("colors.html", color=color)
+
+@app.route('/violet',methods=['GET'])
+def violetPage():
+    color = 'VIOLET'
+    return render_template("colors.html", color = color)
+
+#bottom of the code 
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
